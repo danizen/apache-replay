0b2c6efe4e0fa83ab6263c13a6cad04ca3a8e1ed
Dan Davis <daniel.davis@nih.gov>
2018-11-16 12:00:31 -0500

    Add tox environment to it.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+
+def get_readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name = 'apache-replay',
     version = '0.0.1',
@@ -7,6 +13,8 @@ setup(
     author = 'Daniel Davis',
     author_email = 'dan@danizen.net',
     description = 'Facilitates replaying of Apache files in Common Log and Combined Log format',
+    long_description = get_readme(),
+    long_description_content_type='text/markdown; charset=UTF-8; variant=CommonMark',
     packages = ['apache_replay'],
     entry_points={
         'console_scripts': [
@@ -14,5 +22,8 @@ setup(
         ]
     },
     install_requires = ['attrs', 'requests'],
-    tests_require = ['attrs', 'requests', 'pytest', 'pytest-pythonpath', 'pytest-cov']
+    tests_require = ['attrs', 'requests', 'pytest', 'pytest-pythonpath', 'pytest-cov', 'tox'],
+    classifiers = [
+
+    ]
 )
